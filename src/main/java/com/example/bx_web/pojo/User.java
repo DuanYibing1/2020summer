057bbbdaddd78c4c7c527acb7e461456830a5f44
duanyibing <duan_yibing@163.com>
2020-07-06 17:33:53 +0800

    SSM部署文件

---
+++ src/main/java/com/example/bx_web/pojo/User.java
@@ -0,0 +1,16 @@
+package com.example.bx_web.pojo;
+
+public class User {
+    public String getUsername(){
+        return null;
+    }
+
+    public void setUsername(String username){
+    }
+
+    public String getPassword(){
+        return null;
+    }
+    public void setPassword(String password){
+    }
+}
